3fac128f7e5ac953733046aad9bd7c403490aba3
razu381 <sirazu52@gmail.com>
2025-02-25 21:55:01 +0600

    stripe payment intent

--- index.js
+++ index.js
@@ -320,6 +320,21 @@ async function run() {
       }
     );
 
+    //------------- payment with stripe goes here ----------------
+    app.post("/create-payment-intent", async (req, res) => {
+      const { price } = req.body;
+      const amount = parseInt(price * 100);
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        payment_method_types: ["card"],
+      });
+
+      res.send({
+        clientSecret: paymentIntent?.client_secret,
+      });
+    });
+
     ///------------- start of jwt ------------------
     app.post("/jwt", async (req, res) => {
       let user = req.body;
